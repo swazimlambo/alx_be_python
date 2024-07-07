db3a4df4b40e469c704900bfc9e14c60186d3c41
Swazi Mlambo <swaziimlambo@gmail.com>
2024-07-07 12:32:09 +0200

    shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,29 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input ("Enter your choice: ")
+        if choice == '1':
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+        elif choice == '2':
+            item = input("Enter the item to remove: ")
+            shopping_list. remove(item)
+        elif choice == '3':
+            for items in (shopping_list):
+                print(items)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else: 
+            print("Invalis choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
