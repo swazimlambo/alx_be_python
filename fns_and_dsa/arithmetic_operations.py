26bcd84fd28a785ad3518c44a0228f6a71cae93f
Swazi Mlambo <swaziimlambo@gmail.com>
2024-07-07 11:50:43 +0200

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operations) :
+    match operation:
+        case 'add' :
+            return num1 + num2
+        case 'subtract' :
+            return num1 - num2
+        case 'multiply' :
+            return num1 * num2
+        case 'divide' :
+            if num2 == 0:
+                print("You cannot divide by zero")
+            elif num2 != 0:
+                return num1 / num2
+            else:
+                return
+        case _:
+            return "Enter add, subtract, multiply or divide keyword"        
