9987922782fa1a301078bdc83edbcc7e366f274b
Swazi Mlambo <swaziimlambo@gmail.com>
2024-07-07 12:52:59 +0200

    explore datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
+
+def calculate_future_date(number_of_days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=number_of_days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Future date: {formatted_future_date}")    
+
+def main():
+    display_current_datetime()    
+
+    try:
+        number_of_days = int(input("Ente the number of days to add to the current date: "))
+        calculate_future_date(number_of_days)
+    except ValueError:
+        print("Invalid input. Please enter an integer")
+
+if __name__ == "__main__":
+    main()        
