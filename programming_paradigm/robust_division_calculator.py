0a3adc24cbffb82d22fcf5bd6da9a0cd9d2675e6
Swazi Mlambo <swaziimlambo@gmail.com>
2024-07-15 00:37:17 +0200

    fourth

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,6 +7,6 @@ def safe_divide(numerator, denominator):
             result = num / denom
             return f"The result of the division is {result}"
         except ZeroDivisionError:
-            return "Error: Cannot deivide by zero."
+            return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
