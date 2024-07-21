f21fb787bd6fb5870ebfc17fab3b49abd15422f6
Swazi Mlambo <swaziimlambo@gmail.com>
2024-07-21 22:42:00 +0200

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,15 +22,15 @@ class PrintBook(Book):
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"   
 
-    class Library:
-        def __init__(self):
-            self.books = []
+class Library:
+    def __init__(self):
+        self.books = []
 
-        def add_book(self, book):
-            self.books.append(book)
+    def add_book(self, book):
+        self.books.append(book)
 
-        def list_books(self):
-            for book in self.books:
-                print(book)         
+    def list_books(self):
+        for book in self.books:
+            print(book)         
 
         
--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,10 +4,10 @@ class Shape:
     def area(self):
         raise NotImplementedError("Subclasses must implement area method")
     
-    class Rectangle(Shape):
-        def __init__(self, length, width):
-            self.length = length
-            self.width = width
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
         def area(self):
             return self.length * self.width
